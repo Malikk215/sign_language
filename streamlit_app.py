5e5cd61f203569f02bd67eb81dd3bb8f372ece52
Malikk215 <diazmalikrabbani03@gmail.com>
2025-08-29 00:06:58 +0700

    second commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,7 @@ class SignLanguageDetector:
     def __init__(self):
         self.mp_hands = mp.solutions.hands
         self.hands = self.mp_hands.Hands(
-            static_image_mode=True,
+            static_image_mode=False,
             max_num_hands=1,
             min_detection_confidence=0.5,
             min_tracking_confidence=0.5
